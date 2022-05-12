4c92f0a8fff53b7da39dfd797b2031587ecf7cb7
Thakkar123 <ajit.thakkar17@gmail.com>
2022-05-12 14:52:29 +0100

    First Update

---
+++ src/test/java/com/bank/AppTest.java
@@ -0,0 +1,18 @@
+package com.bank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/resources/testdata/TestData.java
@@ -0,0 +1,16 @@
+package resources.testdata;
+
+public class TestData {
+
+//    @DataProvider(name = "credentials")
+//    public Object[][] getData(){
+//        Object[][] data = new Object[][]
+//            {
+//                {"","123456","An email address required."},
+//                {"abcd@gmail.com", "", "Password is required."},
+//                {"adfdfgfg", "123456", "Invalid email address."},
+//                {"abcd@gmail.com", "123456", "Authentication failed."}
+//            };
+//        return data;
+//    }
+}
